7ba7b01a6c34a46864e6a0aa9125f9c74f0fbb52
purushoath02 <purushoath02@gmail.com>
2022-11-18 18:29:32 +0530

    NA

--- HiQ/core/models.py
+++ HiQ/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class profile(models.Model): 
+    user: models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user: models.IntegerField()
+    profile_img = models.ImageField(upload_to='profile_img', default='default/profile.png')
+    bio = models.TextField(max_length=500, blank=True)
+    def __str__(self): 
+        return self.user.username
+     
